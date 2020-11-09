2696ab1623f91f36119ebc37edb4b708a085aa5e
Daniel Ford <daniel.fordd@gmail.com>
2020-11-08 19:36:53 -0500

    expanded seed data for cocktail tags

--- client/src/components/IngredientsSearch/IngredientSearchContainer.js
+++ client/src/components/IngredientsSearch/IngredientSearchContainer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import SearchBar from './IngredientSearchBar'
 import IngredientSearchResults from './IngredientSearchResults'
 import AddedIngredientList from './AddedIngredientList'
 
