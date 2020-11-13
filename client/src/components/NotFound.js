fbdc706adfc92ebd324e4889334400523cc16b2e
Daniel Ford <daniel.fordd@gmail.com>
2020-11-13 16:22:14 -0500

    heroku youtube

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -7,6 +7,9 @@ const NotFound = () => {
             <h1>You missed last call, no cocktails found.</h1>
         </div>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/3xzDm5v7lXI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ZP50OnqSwMM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/EAS-pvQ06s4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/usKwQwU0wQg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
